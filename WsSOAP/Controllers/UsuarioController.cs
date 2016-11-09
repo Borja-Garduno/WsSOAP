e0006a66ce75c5e408fd3ed05f2017579ac09d92
Tamasco <tamasco_tau@hotmail.com>
2016-11-09 16:22:32 +0100

    SOAP Client

---
+++ WsSOAP/Controllers/UsuarioController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WsSOAP.SOAPService;
+
+namespace WsSOAP.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        // GET: Usuario
+        public ActionResult Index()
+        {
+            
+            SOAPService.UsuarioClient service = new SOAPService.UsuarioClient();
+            IList<Usuario> usuarios = service.getAll();
+
+
+            return View(usuarios);
+        }
+    }
+}
